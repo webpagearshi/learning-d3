6ca52622143672872e092213c589823a49894ec8
Arshi Saleh <arshisaleh@gmail.com>
2026-03-20 18:12:28 +0530

    switch to HashRouter for Github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import BarChart from "./pages/lessons/BarChart";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="bar-chart" element={<BarChart />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
