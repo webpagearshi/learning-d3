8e3173ba702eee3a1599ad0a10dc55a417e79f5c
Arshi Saleh <arshisaleh@gmail.com>
2026-03-28 23:16:27 +0530

    wip bar chart

--- src/components/HorizontalBarPlot.jsx
+++ src/components/HorizontalBarPlot.jsx
@@ -1,26 +1,33 @@
 import React from "react";
-import { scaleBand, scaleLinear } from "d3";
+import { scaleBand, scaleLinear, max } from "d3";
 
-function HorizontalBarPlot({ data, width = 500, height = 600 }) {
+function HorizontalBarPlot({ data, width = 650, height = 400 }) {
+  //sort data by count
+  const sortedData = [...data].sort((a, b) => b.count - a.count);
   //create a scale for xAxis
-  const xScale = scaleLinear().domain([0, 55]).range([0, width]);
+  const xScale = scaleLinear()
+    .domain([0, max(sortedData, (d) => d.count)])
+    .range([0, width]);
 
   //create a scale for yAxis
   const yScale = scaleBand()
-    .domain(data.map((d) => d.name))
+    .domain(sortedData.map((d) => d.name))
     .range([0, height])
-    .padding(0.2);
+    .padding(0.3);
 
   return (
-    <svg width={width} height={height}>
-      {data.map((d, i) => (
+    <svg width={width} height={height} style={{ overflow: "visible" }}>
+      {sortedData.map((d, i) => (
         <g key={i}>
           <rect
             x={0}
             y={yScale(d.name)}
             width={xScale(d.count)}
             height={yScale.bandwidth()}
+            shapeRendering="crispEdges"
+            fill="#076fa2"
           />
+          <line x1={0} y1={12} x2={0} y2={390} stroke="#808080" />
         </g>
       ))}
     </svg>
